a2ff9c68d5c7f251690e0181d82ce39cf7657b81
Joe Robbins <joerobbins@Joes-MacBook-Air.local>
2022-11-02 12:06:53 -0400

    commit tree ca52b677da713a7dfd90a48be4a2eafa1e9d4a5a

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+
+require 'pry'
+class Song
+    attr_accessor :name, :artist, :genre
+
+
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @@artists << artist
+        @genre = genre
+        @@genres << genre
+        @@count += 1
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def genre_count
+        @genres
+    end
+
+end
+
+# binding.pry
